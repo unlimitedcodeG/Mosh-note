8ee8da22706cac93b09f2f7dcecaf89a19034bbd
Unlimited CODE <48639567+unlimitedcodeG@users.noreply.github.com>
2019-10-20 10:23:30 +0800

    Add files via upload

--- JSexercise/Object exercise/Object equality.js
+++ JSexercise/Object exercise/Object equality.js
@@ -2,7 +2,7 @@ let address1=new Address('a','b','c');
 let address2=new Address('a','b','c');//the address is not as a same address
 let address3=address1;//this is a same
 console.log(areEqual(address1,address2))//true
-console.log(areSame(address1,address2))//true
+console.log(areSame(address1,address2))//false
 console.log(areSame(address1,address3))//true
 //Constructor Function
 function Address(street,city,zipCode){
@@ -17,6 +17,6 @@ function areEqual(address1,address2){
     
 }
 function areSame(address1,address2){
-    return address1===address2;
+    return address1===address2;//compare the address
 
 }
